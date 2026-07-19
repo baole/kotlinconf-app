8625fc3a72377143de425633246c50f475621b8b
baole <leducbao@gmail.com>
2026-07-19 12:07:20 +0200

    Bump Konture version to 0.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     alias(libs.plugins.jib) apply false
     alias(libs.plugins.googleServices) apply false
     alias(libs.plugins.metro) apply false
-    id("io.github.baole.konture") version "0.6.10"
+    id("io.github.baole.konture") version "0.7.0"
 }
 
 // Pin Node.js to the last 22.x LTS. Node 24/25 prebuilt linux-x64 binaries
