300f46432b90c5bba0fd9880e8f7288fa3f36f41
baole <leducbao@gmail.com>
2026-07-24 17:40:17 +0200

    Bump Konture version to 0.7.3

--- konture-test/build.gradle.kts
+++ konture-test/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     // Standard test implementation of the unified Konture assertion library
-    testImplementation("io.github.baole:konture:0.7.2")
+    testImplementation("io.github.baole:konture:0.7.3")
 
     // JUnit 5 test dependencies
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
